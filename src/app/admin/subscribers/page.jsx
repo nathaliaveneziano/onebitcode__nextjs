b94e3d6ffe2d4a9854606b1f31edf3ff4f4aedaf
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-08-15 18:45:52 -0300

    custom table with data

---
+++ src/app/admin/subscribers/page.jsx
@@ -0,0 +1,33 @@
+import mysql from 'mysql2/promise';
+
+export default async function Subscribers() {
+  const connection = await mysql.createConnection(
+    'mysql://nextjs:nextjs@localhost:3306/next13'
+  );
+  const [rows] = await connection.query('SELECT * FROM Subscribers;');
+  connection.end();
+
+  return (
+    <main>
+      <h1 className="text-2xl font-bold mb-8">Lista de inscritos</h1>
+      <table className="w-full text-center">
+        <thead className="border-b-[1px]">
+          <tr className="[&>*]:py-4">
+            <th>ID</th>
+            <th>Email</th>
+            <th>Created At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((subscriber) => (
+            <tr key={subscriber.id} className="[&>*]:p-4">
+              <td>{subscriber.id}</td>
+              <td className="text-left">{subscriber.email}</td>
+              <td>{subscriber.createdAt.toDateString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+}
